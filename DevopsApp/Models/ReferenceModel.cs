96d0302c9b3780cd36703a152e2becd63ab599e6
Harish Vanapalli <harish.vanapalli@nagarro.com>
2024-03-29 16:28:23 +0530

    made some changes in code

--- DevopsApp/Models/ReferenceModel.cs
+++ DevopsApp/Models/ReferenceModel.cs
@@ -4,11 +4,11 @@ namespace DevopsApp.Models
 {
     public class ReferenceModel
     {
-        public string Title { get; set; }
-        public string Category { get; set; }
-        public string Content { get; set; }
-        public string Description { get; set; }
-        public string Author { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Title { get; set; }
+        public string? Category { get; set; }
+        public string? Content { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
